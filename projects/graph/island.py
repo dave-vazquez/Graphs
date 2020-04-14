11b30fa988f5d1bcca27d6e6114320c2baf69e36
Dave Vazquez <dmvaz.li@gmail.com>
2020-04-14 15:31:12 -0400

    island.py, some fun..

--- projects/graph/island.py
+++ projects/graph/island.py
@@ -1,4 +1,7 @@
+import pprint
+import time
 import os
+p_print = pprint.PrettyPrinter(width=30).pprint
 os.system("clear")
 
 islands = [[0, 1, 0, 1, 0],
@@ -6,6 +9,10 @@ islands = [[0, 1, 0, 1, 0],
            [0, 0, 1, 0, 0],
            [1, 0, 1, 0, 0],
            [1, 1, 0, 0, 0]]
+
+print("A happy little archipelago...\n")
+p_print(islands)
+print("")
 
 
 def sink_all_islands(islands):
@@ -15,10 +22,17 @@ def sink_all_islands(islands):
         for x in range(0, len(islands[y])):
             # if the node is an island-node
             if islands[y][x] == 1:
+                # (this line is just for fun)
+                # can't sink an island if you only sink it's connected nodes..
+                islands[y][x] == 0
                 # sink all the connected island-nodes
                 islands = sink_island(x, y, islands)
                 # and increment the islands sunk by 1
                 islands_sunk += 1
+                time.sleep(3)
+                print(f"{islands_sunk} island(s) sunk...\n")
+                p_print(islands)
+                print("")
 
     return islands_sunk
 
@@ -55,8 +69,12 @@ def sink_island(x, y, islands):
 
 
 islands_sunk = sink_all_islands(islands)
+time.sleep(3)
+print(f"islands sunk: {islands_sunk}\n")
 
-print(f"islands sunk: {islands_sunk}")
+print("A happy little archipelago no more...\n")
+p_print(islands)
+print("")
 
 '''''''''''''''''''''''''''''''''''''''''''''''''''''
                    INSTRUCTIONS
