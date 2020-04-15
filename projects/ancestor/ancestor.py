09e50146df84f48f35ad315f238d7827d33a4ed4
Dave Vazquez <dmvaz.li@gmail.com>
2020-04-15 11:00:27 -0400

    ancestory.py, adds comments

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -7,20 +7,31 @@ p_print = pprint.PrettyPrinter(width=30).pprint
 def earliest_ancestor(ancestors, starting_node):
     graph = Graph()
 
+    # add the vertices
     for pair in ancestors:
         graph.add_vertex(pair[0])
         graph.add_vertex(pair[1])
 
+    # add the edges, with pair order reversed
     for pair in ancestors:
         graph.add_edge(pair[1], pair[0])
 
+    # get all paths where any duplicate lengthed paths
+    # are reduced to 1 path with the earliest ancestor of
+    # the smallest id
     all_paths = graph.dft_paths(starting_node)
 
+    # if there is only one path, ex. [6]
+    # there must be no ancestors, therefore
+    # return -1
     if len(all_paths) == 1:
         return -1
 
+    # otherwise select the longest_path using the
+    # length of all_paths as the key
     longest_path = all_paths[len(all_paths)]
 
+    # and return the earliest ancestor of that path
     return longest_path[-1]
 
 
